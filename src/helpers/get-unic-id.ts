cc6374609b4d2332ef47bec16e6c16638f4f2aaf
Dmitry Khitrin <khitrind@yandex-team.ru>
2020-10-27 13:29:34 +0300

    Added descr for id generator.

--- src/helpers/get-unic-id.ts
+++ src/helpers/get-unic-id.ts
@@ -1,3 +1,6 @@
+/**
+ * @description Possible improvements for this function, adding a unique hash for each instance of the form.
+ */
 const generateUniqId = () => {
     let id = 0;
     return () => String(id++);
